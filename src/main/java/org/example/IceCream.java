d611ec9447e8cf9f18630fa31e6f72015113e96a
chryswds <dybas.chrys@gmail.com>
2024-10-15 10:39:40 +0100

    first item class created - Ice cream

---
+++ src/main/java/org/example/IceCream.java
@@ -0,0 +1,21 @@
+package org.example;
+//the first thing to do to make this food available and functional with our system
+
+
+public class IceCream extends Food implements Allergic {
+
+    //Add the allergens for the icecream so that i cant change
+    private final Allergens[] ALLERGENS = {Allergens.DAIRY};
+
+    //icecream is a food, from Food class foods have name price type
+    IceCream(){
+        //The information for this food is passed in here
+        //Super sends the information to the parent to make it accessible to the other classes that read information from the parent
+        super("Ice Cream", 4.5, Courses.DESSERT);
+    }
+    //Override to get rid of the error
+    @Override
+    public Allergens[] listAllergens(){
+        return ALLERGENS;
+    }
+}
